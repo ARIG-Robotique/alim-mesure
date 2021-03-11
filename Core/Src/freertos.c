34a2c420752ce11e475d8d38082332d9f515c1cf
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2021-03-11 10:15:35 +0100

    Test i2c master

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -26,7 +26,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "i2c.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,17 +49,17 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
 /* Definitions for heartBeatTask */
 osThreadId_t heartBeatTaskHandle;
 const osThreadAttr_t heartBeatTask_attributes = {
   .name = "heartBeatTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for i2cTask */
+osThreadId_t i2cTaskHandle;
+const osThreadAttr_t i2cTask_attributes = {
+  .name = "i2cTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
 };
@@ -68,8 +69,8 @@ const osThreadAttr_t heartBeatTask_attributes = {
 
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void *argument);
 void StartHeartBeatTask(void *argument);
+void StartI2CTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -100,11 +101,11 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
   /* creation of heartBeatTask */
   heartBeatTaskHandle = osThreadNew(StartHeartBeatTask, NULL, &heartBeatTask_attributes);
+
+  /* creation of i2cTask */
+  i2cTaskHandle = osThreadNew(StartI2CTask, NULL, &i2cTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -114,24 +115,6 @@ void MX_FREERTOS_Init(void) {
   /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
 
-}
-
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END StartDefaultTask */
 }
 
 /* USER CODE BEGIN Header_StartHeartBeatTask */
@@ -147,10 +130,69 @@ void StartHeartBeatTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
+    printf("heartBeatTask: Toggle LED\n");
     HAL_GPIO_TogglePin(HEART_BEAT_GPIO_Port, HEART_BEAT_Pin);
     osDelay(1000);
   }
   /* USER CODE END StartHeartBeatTask */
+}
+
+/* USER CODE BEGIN Header_StartI2CTask */
+/**
+* @brief Function implementing the i2cTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartI2CTask */
+void StartI2CTask(void *argument)
+{
+  /* USER CODE BEGIN StartI2CTask */
+  printf("i2cTask: Scanning I2C bus:\n");
+  HAL_StatusTypeDef result;
+  uint8_t i;
+  for (i=1; i<128; i++)
+  {
+    /*
+     * the HAL wants a left aligned i2c address
+     * &hi2c1 is the handle
+     * (uint16_t)(i<<1) is the i2c address left aligned
+     * retries 2
+     * timeout 2
+     */
+    result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i<<1), 2, 2);
+//    if (result != HAL_OK) // HAL_ERROR or HAL_BUSY or HAL_TIMEOUT
+//    {
+//      printf("."); // No ACK received at that address
+//    }
+    if (result == HAL_OK)
+    {
+      printf("i2cTask: 0x%X", i); // Received an ACK at that address
+    }
+  }
+  printf("\n");
+
+  /* Infinite loop */
+  while(1)
+  {
+    uint8_t send[2] = {0x4C, 0xA0};
+    HAL_StatusTypeDef result = HAL_I2C_Master_Transmit(&hi2c1, 0x22 << 1, send, sizeof(send), 1);
+    if (result == HAL_OK) {
+      printf("i2cTask: Master transmit\n");
+      uint8_t data[2];
+      result = HAL_I2C_Master_Receive(&hi2c1, 0x22 << 1, data, sizeof(data), 1);
+      if (result == HAL_OK) {
+        printf("i2cTask: Master receive 0x%02X 0x%02X\n", data[0], data[1]);
+      } else {
+        printf("i2cTask: Master receive failed\n");
+      }
+    } else {
+      printf("i2cTask: Master transmit failed\n");
+      printf("Error code %d\n", hi2c1.ErrorCode);
+    }
+
+    osDelay(5000);
+  }
+  /* USER CODE END StartI2CTask */
 }
 
 /* Private application code --------------------------------------------------*/
